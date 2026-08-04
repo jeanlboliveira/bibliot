27b8c40131aa75ac9b96092686815ee9be7d30c8
jeanlboliveira <jeanlucas2k07@gmail.com>
2026-08-04 18:37:20 -0300

    feat(settings): configura database settings para SQLite e PostgreSQL

--- livraria_projeto/settings.py
+++ livraria_projeto/settings.py
@@ -30,8 +30,9 @@ SECRET_KEY = 'django-insecure-ostx!+yfsmbfw5^60u*h$v383$u%h6x@1!##r%$&akl37*favm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+]
 
 # Application definition
 
@@ -84,12 +85,21 @@ WSGI_APPLICATION = 'livraria_projeto.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+USE_SQLITE = os.getenv("USE_SQLITE", "True") == "True"
 
-DATABASES = {
-    "default": dj_database_url.parse(
-        os.getenv("DATABASE_URL")
-    )
-}
+if USE_SQLITE:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+else:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            os.environ["DATABASE_URL"]
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
