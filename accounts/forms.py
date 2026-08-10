a8e1af33f71666404b8dcd6b39cb45d243f16822
jeanlucas2k07 <jeanlucas2k07@gmail.com>
2026-08-10 17:35:48 -0300

    fix(adicionar ao carrnho): Solicita que o usário esteja logado para adicionar item ao carrinho

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,64 +1,82 @@
 from django import forms
-from .models import Usuario, Endereco
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django import forms
+
+from .models import Endereco, Usuario
+
 
 class LoginForm(AuthenticationForm):
     # o username é obrigatório modo a estrutura da classe-mãe
     username = forms.EmailField(
-        label='E-mail',
-        widget=forms.EmailInput(attrs={
-            'class': 'auth-input', 
-            'id': 'email',
-        })
+        label="E-mail",
+        widget=forms.EmailInput(
+            attrs={
+                "class": "auth-input",
+                "id": "email",
+            }
+        ),
     )
 
     password = forms.CharField(
-        label='Senha',
-        widget=forms.PasswordInput(attrs={
-            'class': 'auth-input',
-            'id': 'pass',
-        })
+        label="Senha",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "auth-input",
+                "id": "pass",
+            }
+        ),
     )
 
 
 class CadastroForm(UserCreationForm):
-
     nome = forms.CharField(
-        label='Nome',
-        widget=forms.TextInput(attrs={
-            'class': 'auth-input',
-            'id': 'nome'
-        })
+        label="Nome",
+        widget=forms.TextInput(attrs={"class": "auth-input", "id": "nome"}),
     )
 
     email = forms.EmailField(
-        label='E-mail',
-        widget=forms.EmailInput(attrs={
-            'class': 'auth-input', 
-            'id': 'email',
-        })
+        label="E-mail",
+        widget=forms.EmailInput(
+            attrs={
+                "class": "auth-input",
+                "id": "email",
+            }
+        ),
+    )
+
+    telefone = forms.CharField(
+        label="Telefone",
+        widget=forms.EmailInput(
+            attrs={
+                "class": "auth-input",
+                "id": "telefone",
+            }
+        ),
     )
 
     password1 = forms.CharField(
-        label='Senha',
-        widget=forms.PasswordInput(attrs={
-            'class': 'auth-input',
-            'id': 'password1',
-        })
+        label="Senha",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "auth-input",
+                "id": "password1",
+            }
+        ),
     )
 
     password2 = forms.CharField(
-        label='Confirmar Senha',
-        widget=forms.PasswordInput(attrs={
-            'class': 'auth-input',
-            'id': 'password2',
-        })
+        label="Confirmar Senha",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "auth-input",
+                "id": "password2",
+            }
+        ),
     )
+
     class Meta(UserCreationForm.Meta):
         model = Usuario
-        fields = ('email', 'nome')
-    
+        fields = ("email", "nome")
+
 
 class EnderecoForm(forms.ModelForm):
     class Meta:
